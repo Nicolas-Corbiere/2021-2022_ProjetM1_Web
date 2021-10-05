7ef8ec70aee7588f8ce84de48ef2dcf670255733
Alcaraz Yannick <yannick.alcaraz@etu.unice.fr>
2021-10-05 09:08:52 +0200

    ajout du client v2

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Bienvenue dans HelloWorld : {{msg}}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+      msg : String
+  }
+}
+</script>
+
+
+<style>
+  small {
+    display: block;
+  }
+</style>
