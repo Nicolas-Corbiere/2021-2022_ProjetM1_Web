ff6fb6c6bb6205983c2e7e965d384bc1bd864d79
ncorbiere <nicolas.corbiere@amadeus.com>
2021-10-10 14:43:30 +0200

    Changement du formulaire d'ajout de restaurant #10

--- src/client/src/components/AjoutRestau.vue
+++ src/client/src/components/AjoutRestau.vue
@@ -1,17 +1,130 @@
 <template>
   <div>
-    <h1> Ajout d'un restaurant </h1>
+    <h1>Ajout d'un restaurant</h1>
+
+    <!--
+    <form @submit.prevent="ajouterRestaurant($event)">
+      <label>
+        Nom : <input name="nom" type="text" required v-model="nom" />
+      </label>
+      <br>
+      <label>
+        Cuisine :
+        <input name="cuisine" type="text" required v-model="cuisine" />
+      </label>
+      <br>
+      <button class="md-raised">Ajouter</button>
+    </form>
+    -->
+
+    <form
+      novalidate
+      class="md-layout"
+      @submit.prevent="ajouterRestaurant($event)"
+    >
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Restaurant</div>
+        </md-card-header>
+
+        <md-card-content>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="nom">Nom</label>
+                <md-input name="nom" id="nom" v-model="form.nom" />
+              </md-field>
+            </div>
+
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="cuisine">Cuisine</label>
+                <md-input name="cuisine" id="cuisine" v-model="form.cuisine" />
+              </md-field>
+            </div>
+          </div>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label for="ville">Ville</label>
+                <md-input name="ville" id="ville" v-model="form.ville" />
+              </md-field>
+            </div>
+          </div>
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary"
+            >Ajout Restaurant</md-button
+          >
+        </md-card-actions>
+      </md-card>
+    </form>
   </div>
 </template>
 
 
 <script>
 export default {
-  name: 'AjoutRestau',
-}
+  name: "AjoutRestau",
+  data: () => ({
+    form: {
+      nom: "",
+      cuisine: "",
+      ville: "",
+    },
+    msg: "",
+  }),
+  mounted() {
+    console.log(this.form);
+  },
+  methods: {
+    clearForm() {
+      this.$reset();
+      this.form.nom = "";
+      this.form.cuisine = "";
+      this.form.ville = "";
+    },
+    ajouterRestaurant(event) {
+      // Récupération du formulaire. Pas besoin de document.querySelector
+      // ou document.getElementById puisque c'est le formulaire qui a généré
+      // l'événement
+      let form = event.target;
+
+      // Récupération des valeurs des champs du formulaire
+      // en prévision d'un envoi multipart en ajax/fetch
+      let donneesFormulaire = new FormData(form);
+
+      let url = "http://localhost:8080/api/restaurants";
+
+      fetch(url, {
+        method: "POST",
+        body: donneesFormulaire,
+      })
+        .then((responseJSON) => {
+          responseJSON.json().then((resJS) => {
+            // Maintenant res est un vrai objet JavaScript
+            console.log(resJS.msg);
+            this.msg = resJS.msg;
+          });
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+
+      this.form.nom = "";
+      this.form.cuisine = "";
+      this.form.ville = "";
+    },
+  },
+};
 </script>
 
-
 <style>
-
+.md-progress-bar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+}
 </style>
