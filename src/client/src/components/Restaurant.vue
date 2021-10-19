45af3a44e4211530c0451dd1fc8bebdc70ef545d
ncorbiere <nicolas.corbiere@amadeus.com>
2021-10-19 10:03:19 +0200

    Ajout de la map avec centrage sur position #6

--- src/client/src/components/Restaurant.vue
+++ src/client/src/components/Restaurant.vue
@@ -1,89 +1,145 @@
 <template>
   <div>
-    <h1>Détail du restaurant qui a pour id : {{id}}</h1>
+    <h1>Détail du restaurant qui a pour id : {{ id }}</h1>
 
-    <p class="informarion"> <b>Nom</b> : {{restaurant.name}} &nbsp;&nbsp;&nbsp;&nbsp;
-        <b> Cusine</b> : {{restaurant.cuisine}} &nbsp;&nbsp;&nbsp;&nbsp;
-        <b> Ville </b> : {{restaurant.borough}} </p>
+    <p class="informarion">
+      <b>Nom</b> : {{ restaurant.name }} &nbsp;&nbsp;&nbsp;&nbsp;
+      <b> Cusine</b> : {{ restaurant.cuisine }} &nbsp;&nbsp;&nbsp;&nbsp;
+      <b> Ville </b> : {{ restaurant.borough }}
+    </p>
 
-    <div class="phone-viewport" id="photo" ref="photo" :style="{ backgroundImage: 'url(\'' + restaurant.img + '\')' }">
+    <div
+      class="phone-viewport"
+      id="photo"
+      ref="photo"
+      :style="{ backgroundImage: 'url(\'' + restaurant.img + '\')' }"
+    >
       <md-bottom-bar md-sync-route>
-        <md-bottom-bar-item  @click="changeBG(1)" exact md-type="shift" md-label="Image" md-icon="image" style="max-width: 30%;min-width: 30%;"></md-bottom-bar-item>
-        <md-bottom-bar-item @click="changeBG(2)" md-type="shift" md-label="Map" md-icon="map" style="max-width: 40%;min-width: 40%;"></md-bottom-bar-item>
-        <md-bottom-bar-item @click="changeBG(3)" md-type="shift"  md-label="Menu" md-icon="restaurant_menu" style="max-width: 30%;min-width: 30%;"></md-bottom-bar-item>
+        <md-bottom-bar-item
+          @click="changeBG(1)"
+          exact
+          md-type="shift"
+          md-label="Image"
+          md-icon="image"
+          style="max-width: 30%; min-width: 30%"
+        ></md-bottom-bar-item>
+        <md-bottom-bar-item
+          @click="changeBG(2)"
+          md-type="shift"
+          md-label="Map"
+          md-icon="map"
+          style="max-width: 40%; min-width: 40%"
+        ></md-bottom-bar-item>
+        <md-bottom-bar-item
+          @click="changeBG(3)"
+          md-type="shift"
+          md-label="Menu"
+          md-icon="restaurant_menu"
+          style="max-width: 30%; min-width: 30%"
+        ></md-bottom-bar-item>
       </md-bottom-bar>
     </div>
 
+    <div class="wrapper">
+      <ejs-maps :zoomSettings="zoomSettings"  :centerPosition= 'centerPosition'>
+        <e-layers>
+          <e-layer :layerType="layerType"> </e-layer>
+        </e-layers>
+      </ejs-maps>
+    </div>
   </div>
 </template>
 
 
 <script>
-export default {
-  name: 'Restaurant',
-  props: {
-  },
-  computed: {
-      id(){
-          return this.$route.params.r
-      }
-  },
-  data: function(){
-      return{
-          restaurant: null
-      }
-  },
-  mounted(){     
-      console.log("Avant affichage, on pourra faire un fetch...");
-      console.log("ID = " + this.id)
-      let url = "http://localhost:8080/api/restaurants/" + this.id;
-      fetch(url)
-      .then(reponse => {
-          return reponse.json();
-      }).then(data =>{
-          this.restaurant = data.restaurant;
-      })
-  },
-  methods : {
-      changeBG(number) {
-          let elem = document.getElementById('photo');
+import Vue from "vue";
+import { MapsPlugin /*, MapsComponent*/, Zoom } from "@syncfusion/ej2-vue-maps";
+//import { world_map } from './world-map.js';
+Vue.use(MapsPlugin);
 
-          this.resetPhoto(elem)
-          if (number === 1) {
-            this.setImage(elem)
-          }
-          else if (number === 2){
-            elem.style.background = "red"
-          }
-          else {
-            elem.style.background = "red"
-          }
-          
+export default {
+  name: "Restaurant",
+  props: {},
+  computed: {
+    id() {
+      return this.$route.params.r;
+    },
+  },
+  data: function () {
+    return {
+      restaurant: null,
+      zoomSettings: {
+        enable: true,
+        //toolBars: ["Zoom", "ZoomIn", "ZoomOut", "Pan", "Reset"],
+        zoomFactor: 4
       },
-      resetPhoto(elem) {
-        elem.style.background = ""
-        elem.style.backgroundImage = "";
-      },
-      setImage(elem){
-        elem.style.backgroundImage = "url('" + this.restaurant.img + "')"
+      centerPosition: {
+          latitude: 0,
+          longitude: 0,
+       },
+      layerType: "OSM",
+    };
+  },
+  provide: {
+    maps: [Zoom],
+  },
+  mounted() {
+    console.log("Avant affichage, on pourra faire un fetch...");
+    console.log("ID = " + this.id);
+    let url = "http://localhost:8080/api/restaurants/" + this.id;
+    fetch(url)
+      .then((reponse) => {
+        return reponse.json();
+      })
+      .then((data) => {
+        console.log(data.restaurant)
+        this.restaurant = data.restaurant;
+        this.centerPosition.latitude = this.restaurant.address.coord[0]
+        this.centerPosition.longitude = this.restaurant.address.coord[1]
+        console.log(this.centerPosition.latitude)
+        console.log(this.centerPosition.longitude)
+      });
+  },
+  methods: {
+    changeBG(number) {
+      let elem = document.getElementById("photo");
+      this.resetPhoto(elem);
+      if (number === 1) {
+        this.setImage(elem);
+      } else if (number === 2) {
+        elem.style.background = "red";
+      } else {
+        elem.style.background = "red";
       }
-  }
-}
+    },
+    resetPhoto(elem) {
+      elem.style.background = "";
+      elem.style.backgroundImage = "";
+    },
+    setImage(elem) {
+      elem.style.backgroundImage = "url('" + this.restaurant.img + "')";
+    },
+  },
+};
 </script>
 
 
 <style>
 .phone-viewport {
-    width: 800px;
-    height: 800px;
-    display: inline-flex;
-    align-items: flex-end;
-    overflow: hidden;
-    border: 1px solid rgba(#000, .26);
-    background: rgba(#000, .06);
-    background-size : 150%;
-  }
-  .informarion{
-    font-size: 2em;
-  }
+  width: 800px;
+  height: 800px;
+  display: inline-flex;
+  align-items: flex-end;
+  overflow: hidden;
+  border: 1px solid rgba(#000, 0.26);
+  background: rgba(#000, 0.06);
+  background-size: 150%;
+}
+.informarion {
+  font-size: 2em;
+}
+.wrapper {
+  max-width: 400px;
+  margin: 0 auto;
+}
 </style>
